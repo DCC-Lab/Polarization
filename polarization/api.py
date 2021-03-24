f915dd6ad8021447a031898803162807d0670fe9
Daniel <dccote@cervo.ulaval.ca>
2021-03-24 15:35:28 -0400

    Create api.py

---
+++ polarization/api.py
@@ -0,0 +1,42 @@
+
+
+class TissueLayer:
+
+    def propagateThrough(self, vector) -> JonesVector:
+
+    def propagateManyThrough(self, vectors) -> [JonesVector]:
+
+    def backscatter(self, vector) -> JonesVector:
+
+    def backscatterMany(self, vectors) -> [JonesVector]:
+
+class TissueStack:
+
+    self.tissueLayers = []
+
+    def propagateThrough(self, vector) -> JonesVector:
+
+    def propagateManyThrough(self, vectors) -> [JonesVector]:
+
+    def backscatter(self, vector) -> JonesVector:
+
+    def backscatterMany(self, vectors) -> [JonesVector]:
+
+    def __iter__(self):
+        return iteer(self.tissueLayers)
+
+
+class Pulse:
+    def __init__(self, vectors = None, centerWavelength = None):
+        self.vectors = vectors
+    # must behave list-like for JonesVectors
+    def __iter__(self):
+        return iter(self.vectors)
+
+    def __next__(self):
+
+    def __len__(self):
+
+
+
+
