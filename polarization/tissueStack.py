227e1e41898974db373bdbfb52c6a77e94d58a87
JLBegin <ludovick.begin@gmail.com>
2021-03-26 14:45:06 -0400

    Update tissueStack.py

--- polarization/tissueStack.py
+++ polarization/tissueStack.py
@@ -3,8 +3,14 @@ import numpy as np
 
 
 class TissueStack:
-    def __init__(self):
+    def __init__(self, offset=0):
         self.layers = []
+        self.offset = offset
+        self.layerPositions = [self.offset]
 
     def append(self, layer: TissueLayer):
+        self.layerPositions.append(self.layerPositions[-1] + layer.thickness)
         self.layers.append(layer)
+
+    def __iter__(self):
+        return self.layers.__iter__()
