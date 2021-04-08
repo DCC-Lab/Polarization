a1247d3c4c4ca84f3649286d07bb33c5359b56a4
JLBegin <ludovick.begin@gmail.com>
2021-04-08 19:29:12 -0400

    TestsTissue: try to get PSOCT signal
    the output seems off but emptyLayer propagation is missing and the resolution is set extremely low (20 instead of 512) because of how slow the computations are

--- polarization/tests/testsTissue.py
+++ polarization/tests/testsTissue.py
@@ -1,10 +1,13 @@
 import envtest
+import numpy as np
 from polarization.tissue import *
+from polarization.pulse import Pulse
+import matplotlib.pyplot as plt
 
 np.random.seed(521)
 
 
-class TestTissueLayer(envtest.MyTestCase):
+class TestTissue(envtest.MyTestCase):
     def setUp(self) -> None:
         pass
 
@@ -17,3 +20,31 @@ class TestTissueLayer(envtest.MyTestCase):
 
         stack = tissue.stackAt(5)
         self.assertAlmostEqual(len(stack), 6)
+
+    def testPSOCT(self):
+        resolution = 20
+        centerWavelength = 1.3
+        bandwidth = 0.13
+
+        tissue = RandomTissue2D(width=3, surface=False, nLayers=1, layerHeightRange=(80, 100))
+        # todo: implement offset as a empty propagation
+        p1 = Pulse.horizontal(centerWavelength, bandwidth, resolution=resolution)
+        p2 = Pulse.leftCircular(centerWavelength, bandwidth, resolution=resolution)  # todo: define a pulse collection instead
+
+        # todo: better implement the following in oop
+        # this is (too slow)**4
+        fringes = np.zeros((tissue.width, 4, resolution), dtype=np.complex)
+        for i, stack in enumerate(tissue):
+            print(i, 1)
+            e1 = stack.backscatterMany(p1)
+            print(i, 2)
+            e2 = stack.backscatterMany(p2)
+            fringes[i] = np.asarray([e1.Ex, e1.Ey, e2.Ex, e2.Ey])
+
+        intensityFringes = 10 * np.log10(np.abs(fringes ** 2))
+
+        plt.imshow(intensityFringes[:, 0])
+        plt.show()
+
+        # p1Out = tissue.scan(p1)
+        # p2Out = tissue.scan(p2)
