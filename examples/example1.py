ddb55e24d89d51dae5fe64588a40b348ba7ab174
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2021-08-18 11:32:48 -0400

    separationexamples

---
+++ examples/example1.py
@@ -0,0 +1,19 @@
+TITLE       = "Linear Polarized Light"
+DESCRIPTION = """Here we can define differently orientated linear polarized light
+ based on the different parameters of the Jones Vectors"""
+
+
+
+from polarization import *
+
+def exampleCode(comments=None):
+    print("\n\nDefine horizontal polarization with JonesVector(Ex=1, Ey=2)")
+    print("===========================================================")
+    v = JonesVector(Ex=1, Ey=0)  # horizontal
+    print("Components are {0}".format(v))
+    print("Orientation is {0:.2f} rad or {1:.1f}°".format(v.orientation, v.orientation * degPerRad))
+    v.show()
+    # v.show("horizontal.mp4") #to save movie
+
+if __name__ == "__main__":
+	exampleCode()
