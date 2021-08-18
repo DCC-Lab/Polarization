aa90c0287127c934b9929851eae0513060443ac5
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2021-08-18 15:21:32 -0400

    update

--- examples/ex02.py
+++ examples/ex02.py
@@ -6,7 +6,7 @@ JonesVector.plus45()
 JonesVector.minus45()
 JonesVector.rightCircular()
 JonesVector.leftCircular()
-here we show two rightCircular and lefttCircular polarization states"""
+here we show two rightCircular and leftCircular polarization states"""
 
 
 
@@ -14,18 +14,12 @@ from polarization import *
 
 def exampleCode(comments=None):
     print("\nJonesVector.rightCircular()")
-    print("-----------------------------")
     v1 = JonesVector.rightCircular()
     print("Components are {0}".format(v1))
     print("Orientation is {0:.2f} rad or {1:.1f}°".format(v1.orientation,v1.orientation*degPerRad))
     v1.show()
-
-if __name__ == "__main__":
-    exampleCode()
-
-def exampleCode(comments=None):
-    print("\nJonesVector.leftCircular()")
     print("-----------------------------")
+    print("\nJonesVector.leftCircular()")
     v2 = JonesVector.leftCircular()
     print("Components are {0}".format(v2))
     print("Orientation is {0:.2f} rad or {1:.1f}°".format(v2.orientation,v2.orientation*degPerRad))
