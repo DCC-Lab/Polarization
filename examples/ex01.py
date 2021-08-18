aed2985b91de6f167f84eeda7d9e6f498b0cadc4
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2021-08-18 12:59:43 -0400

    updateexamples

--- examples/ex01.py
+++ examples/ex01.py
@@ -1,6 +1,7 @@
 TITLE       = "Linear Polarized Light"
 DESCRIPTION = """Here we can define differently orientated linear polarized light
- based on the different parameters of the Jones Vectors"""
+ based on the different parameters of the Jones Vectors, the vector can be normalized with .normalize()
+ e.g. v = JonesVector(Ex=1, Ey=1).normalize() which is +45° polarized """
 
 
 
---
+++ examples/ex02.py
@@ -0,0 +1,36 @@
+TITLE       = "Predefined polarization states using Jones Vector"
+DESCRIPTION = """Many predefined vectors exist such as
+JonesVector.vertical()
+JonesVector.horizontal()
+JonesVector.plus45()
+JonesVector.minus45()
+JonesVector.rightCircular()
+JonesVector.leftCircular()
+here we show two rightCircular and lefttCircular polarization states"""
+
+
+
+from polarization import *
+
+def exampleCode(comments=None):
+    print("\nJonesVector.rightCircular()")
+    print("-----------------------------")
+    v1 = JonesVector.rightCircular()
+    print("Components are {0}".format(v1))
+    print("Orientation is {0:.2f} rad or {1:.1f}°".format(v1.orientation,v1.orientation*degPerRad))
+    v1.show()
+
+if __name__ == "__main__":
+    exampleCode()
+
+def exampleCode(comments=None):
+    print("\nJonesVector.leftCircular()")
+    print("-----------------------------")
+    v2 = JonesVector.leftCircular()
+    print("Components are {0}".format(v2))
+    print("Orientation is {0:.2f} rad or {1:.1f}°".format(v2.orientation,v2.orientation*degPerRad))
+    v2.show()
+
+if __name__ == "__main__":
+    exampleCode()
+
